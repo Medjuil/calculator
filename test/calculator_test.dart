0a90ff152c8579f7d3fb8a73ffb62656057f20b9
Jay Emman <jaydegamo22@gmail.com>
2024-02-27 12:17:04 +0800

    initial

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -36,34 +36,6 @@ test('Addition: Given two negative numbers, When add method is called, Then the 
 });
 
 
-
-  group('Multiplication', () {
-    test('Given Calculator class, when multiplication is called and both numbers are positive, then output positive number for multiplying positive numbers', () {
-      expect(calculatorRepository.multplication(2, 3), equals(6));
-    });
-
-    test('Given Calculator class, when multiplication is called and both numbers are negative, then output positive number for multiplying positive numbers', () {
-      expect(calculatorRepository.multplication(-2, -3), equals(6));
-    });
-
-    test('Given Calculator class, when multiplication is called and both are mixed signs, then output negative number for multiplying mixed signed numbers', () {
-      expect(calculatorRepository.multplication(2, -3), equals(-6));
-    });
-
-    test('Given Calculator class, when multiplication is called and there is a zero, then output zero', () {
-      expect(calculatorRepository.multplication(0, 5), equals(0));
-    });
-
-    test('Given Calculator class, when multiplication is called and there is a 1, then output same number multiplied to one', () {
-      expect(calculatorRepository.multplication(1, 5), equals(5));
-    });
-
-    test('Given Calculator class, when multiplication is called and there is a decimal number, then output correct product for multiplying floating-point numbers', () {
-      expect(calculatorRepository.multplication(2.5, 4), equals(10));
-    });
-  });
-
-
     group('Subtraction', () {
 
     test('Given Calculator class, When subtracting two positive numbers, then return positive output', () {
