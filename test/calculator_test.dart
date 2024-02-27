989e5ea404f62db714ac15e493b02abb1c19aaca
Jay Emman <jaydegamo22@gmail.com>
2024-02-27 11:13:37 +0800

    2nd commit

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -1,18 +1,33 @@
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:calculator/calculator/data/models/calculator.dart';
 
 void main() {
-  group('Calculator Tests', () {
-    test('Given Calculator class, when subtraction is called and 1st number is greater than the 2nd, then output positive number', () {
-      // Arrange
-      final calculator = Calculator();
-      final num1 = 10.0; 
-      final num2 = 5.0; 
-      // Act
-      final result = calculator.subtract(num1, num2);
-      // Assert
-      expect(result, isPositive); 
+
+  group('Calculator', () {
+    final Calculator calculator = Calculator();
+
+    test('Given Calculator class, When subtracting two positive numbers, then return positive output', () {
+      expect(calculator.subtract(20.0, 10.0), 10.0);
+    });
+
+    test('Given Calculator class, When subtracting two negative numbers, then return negative output', () {
+      expect(calculator.subtract(-5.0, -3.0), -2.0);
+    });
+
+    test('Given Calculator class, When subtracting two the same numbers, then return 0 output', () {
+      expect(calculator.subtract(15.0, 15.0), 0.0);
+    });
+
+    test('Given Calculator class, When subtracting zero from a number, then return the number', () {
+      expect(calculator.subtract(10.0, 0.0), 10.0);
+    });
+
+    test('Given Calculator class, When subtracting number from a zero, then return the number with negative sign', () {
+      expect(calculator.subtract(0.0, 5.0), -5.0);
+    });
+
+    test('Given Calculator class, When subtracting with floating points, then return the approximate number output', () {
+      expect(calculator.subtract(10.55, 5.25), moreOrLessEquals(5.3, epsilon: 0.001));
     });
   });
 }
